ad1ae93b6543d802e95b11d185d2ac0b73c5ffac
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-05-11 15:26:07 +0200

    enlazar files

---
+++ src/main/static/js/main.js
@@ -0,0 +1,11 @@
+const toDoList = document.querySelector("#stuffList");
+
+function showList(lists) {
+    lists.forEach(list =>{
+        const element = `<div class="list">
+        <ul>
+            <li></li>
+        </ul></div>`
+    })
+
+}
