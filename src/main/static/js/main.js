12e3195a8e0a3b810e00b5ffdb314a3b80aa7680
Sonia Mestre <mestre.sonia@gmail.com>
2021-05-12 15:10:52 +0200

    user story 1 done

--- src/main/static/js/main.js
+++ src/main/static/js/main.js
@@ -6,14 +6,16 @@ const table = document.getElementById("table");
 function addTask (){
     const row = document.createElement("tr");
         row.innerHTML =`
-            <td> <input type="checkbox"/></td>
+            <td> <input onClick= "borrar()" type="radio"/></td>
             <td> ${input.value}</td>
-            <td> <span onClick="borrar()"></span> </td>
-        `;
+                    `;
         table.appendChild(row);
 }
 boton.addEventListener("click", addTask);
 
-function borrar(event){
+
+function borrar(){
     this.event.target.parentElement.parentElement.remove();
 }
+
+boton.addEventListener("click", borrar);
