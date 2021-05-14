f2652b0469e4b3070971b1076004ba2165573df6
Daniela-Rf5 <danielarodriguezlainez@gmail.com>
2021-05-14 12:19:30 +0200

    tamaños de input y boton

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,22 @@
+//const toDoList = document.querySelector("#stuffList");
+const input = document.getElementById("input");
+const boton = document.getElementById("boton");
+const table = document.getElementById("table");
+
+function addTask (){
+    const row = document.createElement("tr");
+        row.innerHTML =`
+            <td> <input class="circle" onClick= "borrar()" type="radio"/></td>
+            <td> ${input.value}</td>
+                    `;
+        table.appendChild(row);
+        input.value="";
+}
+boton.addEventListener("click", addTask);
+
+
+function borrar(){
+    this.event.target.parentElement.parentElement.remove();
+}
+
+boton.addEventListener("click", borrar);
